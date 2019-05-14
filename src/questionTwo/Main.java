43aa989121ef637e66fe994e0753ad36b4f5c51c
HargunSinghSuri <hargun.15@gmail.com>
2019-05-14 16:54:34 +0530

    Added solution for getting second highest element

---
+++ src/questionTwo/Main.java
@@ -0,0 +1,27 @@
+package questionTwo;
+
+public class Main {
+    public static void main(String args[]) {
+        //Assign value in temp array
+        int[] array = new int[]{10, 20, 30, 40, 50, 5, 1, 7, 100, 65, 5};
+
+        //Assign temp variables
+        int highest = 0, secondHighest = 0, temp = 0;
+
+        //Logic for getting second highest value
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > highest) {
+                temp = highest;
+                highest = array[i];
+                secondHighest = temp;
+            } else if (array[i] > secondHighest) {
+                secondHighest = array[i];
+            }
+        }
+
+        //Printing value for second highest value
+        System.out.println("The highest number is " + highest);
+        System.out.println("The highest number is " + secondHighest);
+
+    }
+}
